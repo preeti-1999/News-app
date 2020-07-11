3e5720ac79f5f9ecaa693e66bafab096b611d2b4
preeti-1999 <preetidey99@gmail.com>
2020-07-11 23:21:57 +0530

    news app upload

---
+++ react-hooks/README.md
@@ -0,0 +1,17 @@
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+To run the application in your system,clone the repo into your system.
+You must have node js installed as a pre-requisite.
+
+Cd into the folder where you cloned the repo.
+
+Type `npm start`
+
+If it does not fetch the news yet,install a CORS extension in your web browser.
+
+The page will be show at port 3000.
---
+++ react-hooks/src/App.js
@@ -0,0 +1,52 @@
+import React, {Component,useState,useEffect} from 'react';
+
+
+
+const App= () => {
+  const[news,setNews]=useState([]);
+  const[searchQuery,setSearchQuery]=useState([]);
+  const[url,setUrl]=useState("http://hn.algolia.com/api/v1/search?query=corona");
+
+  //fetch the news using fetch function which comes default with the browser
+  useEffect(()=>
+    {
+  const fetchNews=() => {
+    fetch(url)
+    .then(result=>result.json())
+    .then(data=>setNews(data.hits))
+    .catch(error=>console.log(error));
+};
+  fetchNews();
+    },
+     [url]);
+
+  const handleChange =(e)=>{
+     setSearchQuery(e.target.value);
+  };
+
+  const handleSubmit= (e)=>{
+    e.preventDefault()
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  }
+
+  return (
+
+       <div style={{background: "grey"}}>
+          <h2 style={{textAlignVertical: "center",textAlign: "center",color:'black'}} >NEWS FILTER</h2>
+          <form onSubmit={handleSubmit}>
+            <input style={{backgroundColor:'#4d4dff'}} type="text" value={searchQuery} onChange={handleChange} />
+             <button style={{width:170,backgroundColor:'#4d4dff',marginTop:20}}>Search</button>
+          </form>
+         <h4 style={{textAlignVertical: "center",textAlign: "center",color:"blue"}}> {news.map((n,i) => (<p key={i}>{n.title}</p>))} </h4>
+
+       </div>
+    );
+  };
+
+  
+     
+
+   
+
+
+export default App;
